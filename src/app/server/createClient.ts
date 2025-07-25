9f382ecadca267112a47572cb8a9cdab636a8cf7
Jess <jlbachel@umich.edu>
2025-07-25 15:23:07 -0400

    goofy and corny. almost pushed secrets to main.

--- src/app/server/createClient.ts
+++ src/app/server/createClient.ts
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABOndnXc_OAYZdsZR-QmNXUFBzOS1fO-w",
-  authDomain: "thelineup-96277.firebaseapp.com",
-  projectId: "thelineup-96277",
-  storageBucket: "thelineup-96277.firebasestorage.app",
-  messagingSenderId: "1022236175488",
-  appId: "1:1022236175488:web:f9f16fb66f2a9d69518a65",
-  measurementId: "G-4RR763BT0G"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
