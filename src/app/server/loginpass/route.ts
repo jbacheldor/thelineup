510ce4b57b892320830ad9bc4ecd0d351ec34a87
Jess <jlbachel@umich.edu>
2025-07-29 17:42:55 -0400

    ok , that was stupid, but i fixed it. go team

--- src/app/server/loginpass/route.ts
+++ src/app/server/loginpass/route.ts
@@ -3,19 +3,19 @@ import { NextRequest, NextResponse } from "next/server";
 import app from "../createClient";
 
 export async function POST(request: NextRequest){
-    let user = null
+    // let user = null
     try {
         const {email, password} = await request.json()
         const auth = getAuth(app);
-        signInWithEmailAndPassword(auth, email, password)
+        const res = signInWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
             // Signed in 
-            user = userCredential.user;
+            let user = userCredential.user;
             // would be cooool, if we could like,,,,
             // gather this data and figure out who exactly is logged in
             // and what level of access they should get
             // i can probably implement this...
-            return NextResponse.json({ user })
+            return user 
         })
         .catch((error) => {
             const errorCode = error.code;
@@ -26,8 +26,8 @@ export async function POST(request: NextRequest){
                 {error: errorMessage},
                 {status: 401}
             )
-        });
-        return NextResponse.json({ user })
+        })
+        return NextResponse.json({ res })
     }catch (e){
         console.log('error in authenticating via password with firebase', e)
         return NextResponse.json(
@@ -35,5 +35,4 @@ export async function POST(request: NextRequest){
             {status: 400}
         )
     }
-    return NextResponse.json({ user })
 }
