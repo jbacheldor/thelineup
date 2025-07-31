b7bb1ba0fdc2a5b2b683dcd6fd42bebdf0f0b8fa
Jess <jlbachel@umich.edu>
2025-07-31 15:22:31 -0400

    adding in login capabilities & updating return from router

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
+'use client'
 import { redirect } from "next/navigation";
 import styles from "./page.module.css";
+import { useState } from "react";
+import Login from "./components/Login";
 
 export default function Home() {
   // if there is no login detected then yes, redirect girly
-  redirect(`/login`)
+  const [token, setToken] = useState<string>("");
+
+  if(!token) {
+    return (
+      <Login setToken={setToken}/>
+    )
+  }
 
   return (
     <div className={styles.page}>
