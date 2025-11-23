5e0bde56fcc49dab7d10c635a1e24a39ebc5f9d7
Jess <jlbachel@umich.edu>
2025-11-23 18:37:52 -0500

    also cls score edit

--- src/app/components/General/Folder.tsx
+++ src/app/components/General/Folder.tsx
@@ -8,19 +8,11 @@ type Props = {
 const Folder:React.FC<Props> = ({onClickEvent, text}) => {
 
     return (
-        <div id="folder">
-                <img className="folder-img" src={"/closed-folder.svg"} onClick={onClickEvent}/>
+        <div id="folder" style={{"margin": "5px","padding": "10px"}}>
+                <img style={{"width": "84px", "height": "65px"}} className="folder-img" src={"/closed-folder.svg"} onClick={onClickEvent}/>
                 <p>{text}</p>
             <style jsx>
             {`
-                img#folder-img {
-                    width: 84px;
-                    height: 65px;
-                }
-                #folder {
-                    margin: 5px;
-                    padding: 10px;
-                }
                 #folder:hover {
                     cursor: pointer;
                 }
