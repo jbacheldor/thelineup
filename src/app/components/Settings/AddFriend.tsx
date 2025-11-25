15c304158e39cd3928d946124e9f9cceb877798e
Jess <jlbachel@umich.edu>
2025-11-25 18:16:30 -0500

    more visual adds to the page

--- src/app/components/Settings/AddFriend.tsx
+++ src/app/components/Settings/AddFriend.tsx
@@ -1,7 +1,11 @@
+'use client'
 
+import { useState } from "react";
+
+const invites = [{name: 'lisa', email: 'lisalovessoccer@gmail.com', date: '13 days ago'}, {name: 'jane', email: 'sexygurl123@hotmail.com', date: '23 days ago'}]
 
 const AddFriend:React.FC = () => {
-
+    const [invitesSent, setInvites] = useState<{name: string, email: string, date: string}[]>(invites)
 
     return (
         <div id="add-friends">
@@ -16,12 +20,49 @@ const AddFriend:React.FC = () => {
             </label>
             
             <button>send invite email</button>
+            <h3>invites sent</h3>
+            <div id="invites">
+                {invites.map((val, index)=> (
+                    <div id="invite" key={index}>
+                        <div id="invite-info">
+                            <p>{val.name}</p>
+                            <p>{val.email}</p>
+                            <p>{val.date}</p>
+                        </div>
+                        <div id="buttons">
+                            <button>resend email</button>
+                            <button>cancel invite</button>
+                        </div>
+                    </div>
+                ))}
+            </div>
             <style jsx>
             {`
+                h3 {
+                    text-decoration: none;
+                    text-align: center;
+                    padding: 10px;
+                }
+                #buttons {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: center;
+                }
                 #add-friends {
                     display: flex;
                     flex-direction: column;
                     margin: 20px;
+                }
+                #invites {
+                    display: flex;
+                    flex-direction: column;
+                }
+                #invite-info {
+                    display: flex;
+                    flex-direction: row;
+                }
+                #invite-info > p {
+                    padding: 2px 6px;
                 }
             `}
             </style>
--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,24 +1,71 @@
 'use client'
 
+import Image from "next/image";
 import AddFriend from "../components/Settings/AddFriend";
 import Friends from "../components/Settings/Friends"
 
+import { AuthContext } from "@/app/context";
+import { useContext, useState } from "react";
+
+type contactForm = {
+    username: string,
+    email: string,
+    number: string
+}
+
+const initialContact:contactForm = {
+    username: '',
+    email: 'eggs@egg.com',
+    number: '313-664-5502'
+}
+
 const Settings:React.FC = () => {
+    const { isAuthenticated } = useContext(AuthContext)
+    const [contact, setContact] = useState(initialContact)
+
+    const [editMode, setEditMode] = useState(false)
 
     // do a query to get all friends 
+
+    const onClick = () => {
+        setEditMode(!editMode)
+    }
+
+    const updateContact = (e: React.FormEvent) => {
+        
+        setContact(
+            {...contact,
+             [(e.target as HTMLInputElement).ariaLabel || '']: (e.target as HTMLInputElement).value   
+            }
+        )
+    }
 
     return (
         <>
         <div id="setting-body">
             <div id="display-settings">
                 <img id="profile-pic" src="./old-windows-screen-saver.jpg"/>
-                <p>Username:</p>
             </div>
             <hr/>
-            <div id="contact-settings">
-                <p>email:</p>
-                <p>phone-number: </p>
-                <button>edit</button>
+            <div id={`contact-settings`} className={`settings-${editMode}`}>
+                {!editMode && <button id="pen-button" onClick={()=>onClick()}><Image alt="pen edit button" height="20" width="20" src="./pen.svg"/></button>}
+                {editMode && <button id="pen-button" onClick={()=>onClick()}>x</button>}
+                <label>
+                    <p>username:</p>
+                    {!editMode && <p>{contact.username}</p>}
+                    {editMode && <input value={contact.username} onChange={(e)=>updateContact(e)} aria-label="username"/>}
+                </label>
+                <label>
+                        <p>email: </p>
+                        {!editMode && <p>{contact.email}</p>}
+                    {editMode && <input value={contact.email} onChange={(e)=>updateContact(e)} aria-label="email"/>}
+                </label>
+                <label>
+                        <p>number: </p>
+                        {!editMode  && <p>{contact.number}</p>}
+                    {editMode &&  <input value={contact.number} onChange={(e)=>updateContact(e)} aria-label="number"/>}
+                </label>
+                {editMode && <button id="save-button" onClick={()=> onClick()}>save</button>}
             </div>
             <hr/>
             <Friends/>
@@ -26,6 +73,19 @@ const Settings:React.FC = () => {
         </div>
         <style jsx>
             {`
+                #save-button {
+                    display: flex;
+                    justify-self: center;
+                    margin: 5px;
+                }
+                button:hover {
+                    cursor: pointer;
+                }
+                #pen-button {
+                    border: none;
+                    background: none;
+                    justify-self: flex-end;
+                }
                 #setting-body {
                     display: flex;
                     flex-direction: column;
@@ -38,6 +98,32 @@ const Settings:React.FC = () => {
                     width: 100px;
                     border: 2px solid white;
                     margin: 10px;
+                }
+                #contact-settings {
+                    width: 230px;
+                    position: relative;
+                }
+                #pen-button {
+                    position: absolute;
+                    top: -15px; 
+                    right: 5px;
+                }
+                #contact-settings > label {
+                    display: flex;
+                    justify-content: center;
+                    margin: 2px;
+                }
+                .settings-true > label {
+                    display: flex;
+                    flex-direction: column;
+                }
+                .settings-false > label {
+                    display: flex;
+                    flex-direction: row;
+                    width: 100%;
+                }
+                input {
+                    padding: 1px 2px;
                 }
             `}
         </style>
