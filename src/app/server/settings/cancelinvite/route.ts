c2bde8912bdc6cd6d4d4813c52dec5fab6a205e1
Jess <jlbachel@umich.edu>
2025-12-15 17:17:12 -0500

    i tihnk i fixed it :-)

--- src/app/server/settings/cancelinvite/route.ts
+++ src/app/server/settings/cancelinvite/route.ts
@@ -1,49 +1,31 @@
-// import { createDBClient } from "@/app/tursoClient";
-// import { NextRequest, NextResponse } from "next/server";
+import { createDBClient } from "@/app/tursoClient";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(){
+export async function PATCH(req: NextRequest){
 
-    // const {id} =  await req.json()
+    const {id} =  await req.json()
 
-    // try {
-    //     const turso = createDBClient()
+    try {
+        const turso = createDBClient()
 
-    //     await turso.execute({
-    //         sql: `UPDATE invites SET status = 'cancelled' WHERE uuid = ?`,
-    //         args: [id]
-    //     })
+        await turso.execute({
+            sql: `UPDATE invites SET status = 'cancelled' WHERE uuid = ?`,
+            args: [id]
+        })
 
             
-    //     return NextResponse.json({
-    //         message: 'Invite successfully cancelled',
-    //         status: '200'
-    //     })
-    // }
-    // catch(error){
-    //     console.log('error: ', error)
-    //     return NextResponse.json({
-    //         error: 'trouble creating db instance',
-    //         status: 400
-    //     })
-    // }
+        return NextResponse.json({
+            message: 'Invite successfully cancelled',
+            status: '200'
+        })
+    }
+    catch(error){
+        console.log('error: ', error)
+        return NextResponse.json({
+            error: 'trouble creating db instance',
+            status: 400
+        })
+    }
     
 
-    // try {
-    //         await turso.execute({
-    //             sql: `UPDATE invites SET status = 'cancelled' WHERE uuid = ?`,
-    //             args: [id]
-    //         })
-
-            
-    //         return NextResponse.json({
-    //             message: 'Invite successfully cancelled',
-    //             status: '200'
-    //         })
-    // } catch(error){
-    //     console.log('error in updating record in turso: ', error)
-    //     return NextResponse.json({
-    //         status: 400,
-    //         message: 'Could not update invite status'
-    //     })
-    // }
 }
