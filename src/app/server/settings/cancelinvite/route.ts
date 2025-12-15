091e9586e49646ccf26fe74aef8ca187bcd509df
Jess <jlbachel@umich.edu>
2025-12-15 16:10:10 -0500

    isolating the route now!

--- src/app/server/settings/cancelinvite/route.ts
+++ src/app/server/settings/cancelinvite/route.ts
@@ -1,31 +1,31 @@
-import { createDBClient } from "@/app/tursoClient";
-import { NextRequest, NextResponse } from "next/server";
+// import { createDBClient } from "@/app/tursoClient";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function PATCH(req: NextRequest){
+export async function PATCH(){
 
-    const {id} =  await req.json()
+    // const {id} =  await req.json()
 
-    try {
-        const turso = createDBClient()
+    // try {
+    //     const turso = createDBClient()
 
-        await turso.execute({
-            sql: `UPDATE invites SET status = 'cancelled' WHERE uuid = ?`,
-            args: [id]
-        })
+    //     await turso.execute({
+    //         sql: `UPDATE invites SET status = 'cancelled' WHERE uuid = ?`,
+    //         args: [id]
+    //     })
 
             
-        return NextResponse.json({
-            message: 'Invite successfully cancelled',
-            status: '200'
-        })
-    }
-    catch(error){
-        console.log('error: ', error)
-        return NextResponse.json({
-            error: 'trouble creating db instance',
-            status: 400
-        })
-    }
+    //     return NextResponse.json({
+    //         message: 'Invite successfully cancelled',
+    //         status: '200'
+    //     })
+    // }
+    // catch(error){
+    //     console.log('error: ', error)
+    //     return NextResponse.json({
+    //         error: 'trouble creating db instance',
+    //         status: 400
+    //     })
+    // }
     
 
     // try {
